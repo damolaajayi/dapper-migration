874111e9517024545ca37fc30f2202dd3fa897ec
damolaajayi <damolaajayi@gmail.com>
2021-09-05 22:03:31 +0100

    first commit

---
+++ Extensions/MigrationManager.cs
@@ -0,0 +1,33 @@
+﻿using DapperMigrations.Migrations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperMigrations.Extensions
+{
+    public static class MigrationManager
+    {
+        public static IHost MigrateDatabase(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var databaseService = scope.ServiceProvider.GetRequiredService<Database>();
+
+                try
+                {
+                    databaseService.CreateDatabase("DapperMigrateExample");
+                }
+                catch
+                {
+
+                    throw;
+                }
+            }
+
+            return host;
+        }
+    }
+}
